b254ab9fa58f5f19f42da8d9ccdd29f7305ec1eb
LRJ <666@qq.com>
2019-05-19 23:04:49 +0800

    Merge branch 'newlocal'
    # Conflicts:
    #	git3/src/Java/hello.java

---
+++ git3/src/Java/hello2.java
@@ -0,0 +1,9 @@
+package Java;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("zhe shi new de fenzhi");
+    }
+}
