70ad620d026a656a5ce3289d8bcc3c9a15b4a1b0
LRJ <666@qq.com>
2019-05-19 22:40:14 +0800

    Merge branch 'newlocal'
    # Conflicts:
    #	git3/src/Java/hello.java

--- git3/src/Java/hello.java
+++ git3/src/Java/hello.java
@@ -6,10 +6,7 @@ public class hello {
 
 
 
-        System.out.println("local");
-        System.out.println("local");
 
-        System.out.println("master");
         System.out.println("master");
 
 
