ee996d47b761059350a342804caf0d2fc3702a94
LRJ <666@qq.com>
2019-05-19 20:45:46 +0800

    修改提交至本地仓库

--- git3/src/Java/hello.java
+++ git3/src/Java/hello.java
@@ -4,5 +4,12 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+
+
     }
 }
