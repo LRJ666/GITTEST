7ec1b57c6dffdd8ce0d40dc8074288dfae599b92
LRJ <666@qq.com>
2019-05-19 22:15:58 +0800

    Merge branch 'newlocal'
    # Conflicts:
    #	git3/src/Java/hello.java

--- git3/src/Java/hello.java
+++ git3/src/Java/hello.java
@@ -3,17 +3,12 @@ package Java;
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
 
-        
 
-  System.out.println("胜多负少发射点发生");
-  System.out.println("胜多负少发射胜多负少发射点发生的点发生");
-  System.out.println("new local  branch");
+
+        System.out.println("hello world");
+        System.out.println("这是主分支");
+
 
     }
 }
