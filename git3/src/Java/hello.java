93326cac144cbc10203b37b4ddb0fe2a022a25fb
LRJ <666@qq.com>
2019-05-19 21:46:17 +0800

    的身份

--- git3/src/Java/hello.java
+++ git3/src/Java/hello.java
@@ -3,18 +3,10 @@ package Java;
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
 
-        
 
-  System.out.println("胜多负少发射点发生");
-
+      
   System.out.println("胜多负少发射胜多负少发射点发生的点发生");
-  System.out.println("new local  branch");
 
     }
 }
